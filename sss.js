61d8a928548223ae861f3f5376853c4d9bb1a3f4
tonchuuu <daitonzzz143@gmail.com>
2024-08-22 02:25:20 +0700

    Add files via upload

---
+++ sss.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Trang web đã được tải xong!');
+    
+    document.body.addEventListener('click', function() {
+        document.body.style.backgroundColor = '#d3d3d3';
+    });
+});
